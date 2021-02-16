06dd973b89e4ff8ecee096168f69ddfd9803725e
Lucas Junior Dias de Paula <lucasmg37@live.com>
2021-02-16 11:20:32 -0300

    add adsense script

--- frontend-nextjs/pages/_app.js
+++ frontend-nextjs/pages/_app.js
@@ -1,8 +1,20 @@
+import React from 'react';
+import Head from 'next/head';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  // eslint-disable-next-line react/react-in-jsx-scope
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <script
+          data-ad-client="ca-pub-7175972143334834"
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
